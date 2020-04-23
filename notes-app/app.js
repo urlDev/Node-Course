95149d09fb1c5ef2f3b74e701167d684b870bebd
Can Ural <can.ural@outlook.com>
2020-04-23 21:32:26 +0300

    made a notes/to do app.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,69 @@
-const chalk = require('chalk');
-const validator = require("validator")
-const getNotes = require('./notes');
+const chalk = require("chalk");
+const yargs = require("yargs");
+const validator = require("validator");
+const notes = require("./notes");
 
-console.log(getNotes())
+// customize the yargs version
+yargs.version("1.1.0");
 
-console.log(validator.isURL("can-ural.com"))
-console.log(chalk.blue.inverse("Success"))
+// create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  // builder is for adding and deleting by title
+  builder: {
+    title: {
+      describe: "Note title",
+      // with this option, it is a required field to add title
+      demandOption: true,
+      // this is option is to get title as a string
+      // empty title would return a boolean value
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  //   argv argument is to add title after the console.log
+  handler(argv) {
+    notes.addNote(argv.title, argv.body);
+  },
+});
+
+// create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    }
+  },
+  handler(argv) {
+    notes.removeNote(argv.title)
+  }
+});
+
+// list command
+yargs.command({
+  command: "list",
+  describe: "List notes",
+  handler() {
+     notes.listNotes()
+  },
+});
+
+// read command
+yargs.command({
+  command: "read",
+  describe: "Read notes",
+  handler(argv) {
+    notes.loadNotes(argv)
+  },
+});
+
+yargs.parse();
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,78 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
 const getNotes = () => {
-    return "your notes is..."
+  return "your notes is...";
+};
+
+const success = chalk.bgGreen;
+const error = chalk.bgRed;
+
+const addNote = (title, body) => {
+  const notes = loadNotes();
+  //   if notes array have same title with the added notes title
+  // then there will be a duplicate
+  // if there is a duplicate, then we wont be able to add
+  const duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title,
+      body,
+    });
+    saveNotes(notes);
+
+    console.log(success("New note added"));
+  } else {
+    console.log(error("Note title taken"));
+  }
+};
+
+const removeNote = (title) => {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter((note) => note.title !== title);
+
+  if (notes.length > notesToKeep.length) {
+    console.log(success("Note removed"));
+  } else {
+    console.log(error("No note found!"));
+  }
+
+  saveNotes(notesToKeep);
+};
+
+const listNotes = (title) => {
+    const notes = loadNotes();
+    console.log(success(`Your notes are:`))
+
+    notes.forEach(note => {
+        console.log(note.title)
+    })
 }
 
-module.exports = getNotes
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+const loadNotes = () => {
+  try {
+    //   in here we get the file from notes.JSON
+    // read it and then make it a string because it was binary
+    // then parse it as JSON
+    // if that file doesnt exist, we return an empty array
+    // to add notes into
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes
+};
