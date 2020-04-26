83747c4f4230ab8102e7c6e1c8d19e28db7dc4c1
Can Ural <can.ural@outlook.com>
2020-04-26 14:33:28 +0300

    Added dynamic pages with handlebars to weather app. Dynamic pages come with views and partials. Views are for pages and partials are for parts in pages like headers and footers. So every page has partials included. Finised making the app, ready to deploy

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,27 @@
-console.log('client side js');
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-one');
+const messageTwo = document.querySelector('#message-two');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  messageOne.innerText = 'Loading...';
+  messageTwo.innerText = '';
+
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) {
+        messageOne.innerText = `${data.error}`;
+      } else {
+        messageOne.innerText = `${data.location}`;
+        messageTwo.innerText = `${data.forecastData}`;
+      }
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+});
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,105 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
-const publicDirPath = path.join(__dirname, '../public');
 
+// define paths for express config
+const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// setup static directory to serve
 app.use(express.static(publicDirPath));
 
 app.get('', (req, res) => {
   res.render('index', {
-    title: 'Weather App',
+    title: 'Weather',
     name: 'Can',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Can',
+    name: 'Can',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'What would you like us to help you about?',
+    title: 'Help',
+    name: 'Can',
+  });
+});
+
+app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address',
+    });
+  } else {
+    geocode(
+      req.query.address,
+      // in here, we set an empty object as default
+      (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+          //   if there is error, we want to return from there
+          // and not to call the other function
+          return res.send({ error });
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+          if (error) {
+            return res.send({ error });
+          }
+
+          res.send({
+            address: req.query.address,
+            location,
+            forecastData,
+          });
+        });
+      }
+    );
+  }
+});
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    // return here will stop the next code from running
+    // if it matches
+    return res.send({
+      error: 'You must provide a search term',
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMessage: 'Help article not found',
+  });
+});
+
+// this matches everything that hasnt been set
+// also it has to be the last page
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMessage: 'Page you are looking for does not exist',
   });
 });
 
@@ -43,12 +132,6 @@ app.get('/about', (req, res) => {
   res.send('<h1>About</h1>');
 });
 
-app.get('/weather', (req, res) => {
-  res.send({
-    forecast: 'Cloudy',
-    location: 'Istanbul',
-  });
-});
 
 */
 
